d8a0a818e7755bf00d3057c974fb8971168e6316
shemajolivetgislain <shemajolivet@gmail.com>
2022-04-09 00:32:12 +0200

    star patterns

---
+++ src/exercises/patterns/starpatterns1.java
@@ -0,0 +1,15 @@
+package exercises.patterns;
+
+public class starpatterns1 {
+    public static void main(String[] args) {
+        //for Rows
+        for (int i=2; i<=7; i++)
+        {
+//            for columns
+            for (int j=7; j>=i; j-=1){
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
