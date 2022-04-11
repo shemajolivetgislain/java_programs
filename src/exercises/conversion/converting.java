9a2dbffd9be216066f55c27a88ccc29e20caa1e3
shemajolivetgislain <shemajolivet@gmail.com>
2022-04-11 09:46:03 +0200

    converting string to integer in java

---
+++ src/exercises/conversion/converting.java
@@ -0,0 +1,9 @@
+package exercises.conversion;
+
+public class converting {
+    public static void main(String[] args) {
+        String a = "123";
+        int sum = Integer.parseInt(a) + 1;
+        System.out.println(sum);
+    }
+}
