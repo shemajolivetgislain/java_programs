c5f499e5f2135de7ce45a33ceafee61bf1a38126
shemajolivetgislain <shemajolivet@gmail.com>
2022-04-27 23:26:40 +0200

    one dimensional

---
+++ src/exercises/oneDimensional.java
@@ -0,0 +1,19 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class oneDimensional {
+    public static void main(String[] args) {
+
+    }
+    public static double getdata(){
+        Scanner input = new Scanner(System.in);
+        double count;
+        double sum = 0;
+        double avg = 0;
+        System.out.print("please en");
+        for (int i= 0;i<=count.l; i++){
+
+        }
+    }
+}
