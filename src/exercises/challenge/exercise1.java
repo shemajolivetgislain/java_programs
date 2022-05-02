dd815e5a33db85613077e6622a100865ab299c81
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-02 22:38:25 +0200

    Challenge 1

---
+++ src/exercises/challenge/exercise1.java
@@ -0,0 +1,12 @@
+package exercises.challenge;
+
+public class exercise1 {
+    public static int AddAndMultiply(int a, int b,int c){
+        int answer = (a+b)*c;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(AddAndMultiply(2, 4, 5));
+    }
+}
