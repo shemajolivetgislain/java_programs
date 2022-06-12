ffa3941efa42cb46d7d47111f8db9a2fa5595486
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-12 22:14:19 +0200

    deletion

--- src/conditions/conditions.java
+++
@@ -1,9 +0,0 @@
-package conditions;
-
-public class conditions {
-    public static void main(String[] args) {
-        int income = 120000;
-        boolean hasHighIcome = income<100000;
-        System.out.println(hasHighIcome);
-    }
-}
