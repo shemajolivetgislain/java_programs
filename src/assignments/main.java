e7a04735e277916fb618bbe54ac159878f1c4d64
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-16 07:36:45 +0200

    adding switch case

--- src/assignments/main.java
+++ src/assignments/main.java
@@ -1,5 +1,8 @@
 package assignments;
 import java.util.Scanner;
+
+import conditions.switchcase;
+
 import static assignments.addPositive.PositiveSummation;
 import static assignments.squareOfPositive.squarePositive;
 import static assignments.sumOfOdd.sumOdd;
@@ -7,11 +10,25 @@ import static assignments.sumOfOdd.sumOdd;
 public class main {
     public static void main(String[] args) {
         int size;
+        int choice;
         Scanner input = new Scanner(System.in);
         System.out.println("enter size of number to looped");
         size = input.nextInt();
-        PositiveSummation(size);
-        sumOdd(size);
-        squarePositive(size);
+        System.out.println("Please your choice");
+        System.out.println("Press 1 to add number which are positive");
+        System.out.println("press 2 to add square of positive numbers");
+        System.out.println("Press 3 to get sum of odd numbers");
+        choice = input.nextInt();
+        switch(choice) {
+            case 1: 
+            PositiveSummation(size);
+            break;
+            case 2: 
+            squarePositive(size);
+            case 3:
+            sumOdd(size);
+        }
+        
     }
+    
 }
